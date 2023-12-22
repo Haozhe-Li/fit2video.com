f90520494ececed9a0788e84c49722cab71d406c
Haozhe-Li <116460778+Haozhe-Li@users.noreply.github.com>
2023-12-22 00:14:47 -0600

    Add files via upload

---
+++ js/util.js
@@ -0,0 +1,29 @@
+
+function changeLanguage(select) {
+let selectedOption = select.options[select.selectedIndex].value;
+window.location.href = selectedOption;
+}
+
+function addDarkmodeWidget() {
+    const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+    }
+    
+    const darkmode = new Darkmode(options);
+    darkmode.showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
+
+function printPage() {
+    window.print();
+  }
